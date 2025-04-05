c104cd64a217c1d557932d7977f3da5d8224b778
subrahmanya <kps.18.1999@gmail.com>
2025-04-05 20:38:06 +0530

    made additional changed on dependency for vercel

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com', 'localhost'],
+  },
   // Add any other Next.js config options here
 };
 
